1f246a0c136e8e7db7dde994f1205a833b765129
Torgen <118271296+vodnokasanie@users.noreply.github.com>
2025-11-02 13:10:50 +0600

    M

--- alb_listener.tf
+++ alb_listener.tf
@@ -1,36 +1,17 @@
 resource "aws_lb_listener" "http_listener" {
-  load_balancer_arn = data.aws_lb.alb.arn
+  load_balancer_arn = aws_lb.alb.arn
   port              = 80
   protocol          = "HTTP"
 
   default_action {
     type = "forward"
-    resource "aws_lb_listener" "http_listener" {
-      load_balancer_arn = aws_lb.alb.arn
-      port              = 80
-      protocol          = "HTTP"
-
-      default_action {
-        type = "forward"
-        forward {
-          target_group {
-            arn    = aws_lb_target_group.blue_tg.arn
-            weight = var.blue_weight
-          }
-          target_group {
-            arn    = aws_lb_target_group.green_tg.arn
-            weight = var.green_weight
-          }
-        }
-      }
-    }
     forward {
       target_group {
-        arn    = data.aws_lb_target_group.blue_tg.arn
+        arn    = aws_lb_target_group.blue_tg.arn
         weight = var.blue_weight
       }
       target_group {
-        arn    = data.aws_lb_target_group.green_tg.arn
+        arn    = aws_lb_target_group.green_tg.arn
         weight = var.green_weight
       }
     }
